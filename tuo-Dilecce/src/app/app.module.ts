503a8893d2cfe34a667fbec816899224e07f5810
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-09-28 20:34:56 +0000

    prep.Verifica

--- tuo-Dilecce/src/app/app.module.ts
+++ tuo-Dilecce/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecondComponentComponent
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent
   ],
   imports: [
     BrowserModule
