17e93e90517ac0c13ea726b15d91ea72bf69895f
Gabriele Di Lecce <90761206+GabrieleGalvani@users.noreply.github.com>
2022-10-10 01:27:23 +0200

    Update app.module.ts

--- prep-verifica/src/app/app.module.ts
+++ prep-verifica/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { MacchinaComponent } from './macchina/macchina.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ConteinerComponent } from './conteiner/conteiner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    MacchinaComponent,
-    ConteinerComponent
+    MacchinaComponent
   ],
   imports: [
     BrowserModule,
